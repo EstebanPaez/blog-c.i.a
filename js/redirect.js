cc1d9325883b273bac96a699e1185c443c75467e
Camilo Paez <cepn26@gmail.com>
2020-10-21 22:10:33 -0500

    Confirmacion

---
+++ js/redirect.js
@@ -0,0 +1,7 @@
+document.querySelector(".post1").addEventListener("click", function () {
+  window.location = "/comunicacion-liquida.html";
+});
+
+document.querySelector(".post2").addEventListener("click", function () {
+  window.location = "/diaz.html";
+});
